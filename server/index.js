b302434b94abfd64554bcd433bfb12324e4abeb4
Ajay Pradhan <pradhanajay14@gmail.com>
2026-03-01 14:52:29 +0530

    git ignoer added

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: ["http://localhost:5174", "http://localhost:5173","http://localhost:3000"],
     credentials: true,
   }),
 );
